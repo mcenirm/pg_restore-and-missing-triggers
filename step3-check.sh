f57f418aa55f02a3e662d681f085dfdb7967a278
Michael McEniry <mcenirm@mceniry.net>
2023-07-26 11:02:47 -0500

    tweak simple checks

--- step3-check.sh
+++ step3-check.sh
@@ -1,11 +1,11 @@
-# set +x
+set +x
 set +e
 
 exit_status=true
 
-c () {
+g () {
   local f=${PGDATABASE}-$1 ; shift
-  if ! grep -q "$@" -- "$f"
+  if ! grep "$@" -- "$f"
   then
     (
       printf '!! grep check failed for file: %q\n++' "$f"
@@ -16,9 +16,9 @@ c () {
   fi
 }
 
-c toc.txt ' FUNCTION public update_updatetime() '
-c toc.txt ' TABLE public t1 '
-c toc.txt ' TABLE DATA public t1 '
-c toc.txt ' TRIGGER public t1 update_t1_updatetime '
+g toc.txt -F -e ' FUNCTION public update_updatetime() '
+g toc.txt -F -e ' TABLE public t1 '
+g toc.txt -F -e ' TABLE DATA public t1 '
+g toc.txt -F -e ' TRIGGER public t1 update_t1_updatetime '
 
 $exit_status
