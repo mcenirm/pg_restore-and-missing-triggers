b9270dd36b3ffe36c368ce08063fcd61fc05e389
Michael McEniry <mcenirm@mceniry.net>
2023-07-26 09:58:59 -0500

    now with tests

---
+++ tests.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE test;
+
+\connect test
+
+CREATE FUNCTION public.update_updatetime()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    NEW.updatetime = now();
+    RETURN new;
+END;
+$$;
+
+CREATE TABLE public.t1 (
+    t1_id       integer PRIMARY KEY GENERATED ALWAYS BY DEFAULT AS IDENTITY,
+    t1_text     text,
+    updatetime  timestamp WITHOUT time zone DEFAULT now()
+);
+
+INSERT INTO public.t1 (t1_text) VALUES ('a');
+INSERT INTO public.t1 (t1_text) VALUES ('b');
+INSERT INTO public.t1 (t1_text) VALUES ('c');
+
+CREATE TRIGGER update_t1_updatetime
+    BEFORE UPDATE ON public.t1
+    FOR EACH ROW
+    EXECUTE FUNCTION public.update_updatetime();
